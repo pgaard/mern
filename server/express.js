87af11ff3e47a1c6a0423a4f0cc42861fa0f848d
Peter Gaard <pgaard@gmail.com>
2021-02-23 16:59:01 -0600

    mongo user and auth

--- server/express.js
+++ server/express.js
@@ -1,21 +1,38 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cookieParser from 'cookie-parser'
-import compress from 'compression'
-import cors from 'cors'
-import helmet from 'helmet'
-import Template from './../template'
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import helmet from "helmet";
+import Template from "./../template";
+import userRoutes from "./routes/user.routes";
+import authRoutes from "./routes/auth.routes";
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).send(Template())
+app.get("/", (req, res) => {
+    res.status(200).send(Template());
 });
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser())
-app.use(compress())
-app.use(helmet())
-app.use(cors())
-export default app
+app.use("/", userRoutes);
+app.use("/", authRoutes);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
+app.use(cors());
+
+
+// Catch unauthorised errors
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+      res.status(401).json({"error" : err.name + ": " + err.message})
+    }else if (err) {
+      res.status(400).json({"error" : err.name + ": " + err.message})
+      console.log(err)
+    }
+  });
+
+export default app;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
-import config from './../config/config';
-import app from './express';
-import mongoose from 'mongoose';
+import config from "./../config/config";
+import app from "./express";
+import mongoose from "mongoose";
 //import { MongoClient } from 'mongodb'
 
-mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, { 
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true } )
-    mongoose.connection.on('error', () => {
-        throw new Error(`unable to connect to database: ${mongoUri}`)
-    }
-);
+    useUnifiedTopology: true,
+});
+mongoose.connection.on("error", () => {
+    throw new Error(`unable to connect to database: ${mongoUri}`);
+});
 /*
 const url = process.env.MONGODB_URI ||
 'mongodb://localhost:27017/mernSimpleSetup'
@@ -21,7 +21,11 @@ db.close()
 */
 app.listen(config.port, (err) => {
     if (err) {
-        console.log(err)
+        console.log(err);
     }
-    console.info('Server started on port %s.', config.port)
+    console.info(
+        "Server started on port %s. mongouri %s ",
+        config.port,
+        config.mongoUri
+    );
 });
